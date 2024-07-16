5c20ca062a8573234ab69f9b98e3d051079734d1
Vítězslav Halfar <halfar.vitezslav@gmail.com>
2024-07-16 15:55:08 +0200

    Fix dimensions and colors

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
-# hackovac
-# Requirements:
-streamlit
-cv2
-numpy
-fpdf
+# Háčkovač
+
+Algoritmus generující předlohy pro filetové háčkování.
+
+# Install
+
+```
+pip install -r requirements.txt
+```
+
 # Run
-streamlit run hackovac.py --server.address ADDRESS --server.port PORT
 
-
+```
+streamlit run hackovac.py --server.address ADDRESS --server.port PORT --browser.serverAddress 127.0.0.1 --server.runOnSave true
+```
--- hackovac.py
+++ hackovac.py
@@ -1,35 +1,41 @@
 import streamlit as st
 import cv2
 import numpy as np
-import fpdf
+# import fpdf
+
+# Constants
+GRID_NORMAL_LINE_SIZE = 1
+GRID_NORMAL_LINE_COLOR_BGR = (80, 80, 80)
+GRID_BOLD_LINE_EVERY = 5
+GRID_BOLD_LINE_SIZE = 2
+GRID_BOLD_LINE_COLOR_BGR = (100, 20, 20)
+
+# User settings
+width_size = st.sidebar.slider('Šířka výstupu', 10, 500, 200)
+threshold = st.sidebar.slider('Práh', 0, 255, 128)
+blur_kernel_size = st.sidebar.slider('Rozmazání', 0, 120, 0) 
+show_original_image = st.sidebar.checkbox('Zobrazit originál', False)
+show_grid = st.sidebar.checkbox('Zobrazit mřížku', False)
+invert = st.sidebar.checkbox('Invertovat barvy', False)
 
 # Load the image
-image = cv2.imread('cat03.jpg')
-image_file = st.file_uploader('Upload an image', type=['jpg', 'jpeg', 'png', 'webp'])
+image = cv2.imread('examples/cat.jpg')
+image_file = st.file_uploader('Výběr souboru', type=['jpg', 'jpeg', 'png', 'webp'])
 if image_file is not None:
     image = cv2.imdecode(np.frombuffer(image_file.read(), np.uint8), 1)
 
-# User settings
-# TODO: Change description to czech, modify value ranges (possibly add fill-in-boxes rather than sliders)
-threshold = st.sidebar.slider('Threshold', 0, 255, 15)
-blur_kernel_size = st.sidebar.slider('blur', 0, 120, 2) 
-show_grid = st.sidebar.checkbox('Show Grid', False)
-invert = st.sidebar.checkbox('Invert', False)
-width_size = st.sidebar.slider('Width of final image', 1, 100, 10)
-show_original_image = st.sidebar.checkbox('Show Original Image', False)
-
-large_line_every = 5 # frequency of bold lines for easier orientation
-                     # TODO: Possibly change to user input?
+image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
 if show_original_image:
-    st.image(image, caption='Uploaded Image')
+    st.image(image)
+    st.write(f'Rozměry: {image.shape[1]} x {image.shape[0]}')
+
 image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
 
-if blur_kernel_size: # TODO: kernel size must be odd (or figure out how gaussian_kernel_size can add padding)
+if blur_kernel_size: # Kernel size must be odd (or figure out how gaussian_kernel_size can add padding)
     if blur_kernel_size % 2 == 0:
         blur_kernel_size += 1
-    image = cv2.Gaussianblur_kernel_size(image, (blur_kernel_size, blur_kernel_size), 0)
-
+    image = cv2.GaussianBlur(image, (blur_kernel_size, blur_kernel_size), 0)
 
 h,w = image.shape
 height_size = int(width_size * h / w)
@@ -37,25 +43,21 @@ height_size = int(width_size * h / w)
 w_ratio = w // width_size
 h_ratio = h // height_size
 
-downscale_image = cv2.resize(image, (height_size, width_size))  # Replace (800, 600) with your desired resolution
+downscale_image = cv2.resize(image, (width_size, height_size))
 d_h, d_w = downscale_image.shape
 
-image = cv2.resize(downscale_image, (height_size * h_ratio, width_size * w_ratio), interpolation=cv2.INTER_NEAREST)
+image = cv2.resize(downscale_image, (width_size * w_ratio, height_size * h_ratio), interpolation=cv2.INTER_NEAREST)
 _, image = cv2.threshold(image, threshold, 255, cv2.THRESH_BINARY)
 
+
 # Add grid to the image based on the downscaled image
-# TODO: 
 if invert:
     image = cv2.bitwise_not(image)
     downscale_image = cv2.bitwise_not(downscale_image)
 
-grid_color = (90, 60, 90)  # Define the color of the grid (in BGR format)
 image = cv2.cvtColor(image, cv2.COLOR_GRAY2RGB)
 
 # Draw grid 
-# TODO: Lots of magical constants here, probably a better way of doing this
-large_line_color = (100, 20, 20)
-small_line_color = (80, 80, 80)
 if show_grid:
     # Calculate the size of each grid cell
     cell_width = image.shape[1] // downscale_image.shape[1]
@@ -63,37 +65,36 @@ if show_grid:
 
     # Draw vertical grid lines
     for x in range(0, downscale_image.shape[1]+1):
-        if x % 5:
-            line_width = 1
-            line_color = small_line_color
+        if x % GRID_BOLD_LINE_EVERY:
+            line_width = GRID_NORMAL_LINE_SIZE
+            line_color = GRID_NORMAL_LINE_COLOR_BGR
         else:
-            line_width = 3
-            line_color = large_line_color
+            line_width = GRID_BOLD_LINE_SIZE
+            line_color = GRID_BOLD_LINE_COLOR_BGR
         x = x * cell_width
         cv2.line(image, (x, 0), (x, image.shape[0]), line_color, line_width)
 
     # Draw horizontal grid lines
     for y in range(0, downscale_image.shape[0]+1):
         y = y * cell_height
-        if y % 5:
-            line_width = 1
-            line_color = small_line_color
+        if y % GRID_BOLD_LINE_EVERY:
+            line_width = GRID_NORMAL_LINE_SIZE
+            line_color = GRID_NORMAL_LINE_COLOR_BGR
         else:
-            line_width = 3
-            line_color = large_line_color
+            line_width = GRID_BOLD_LINE_SIZE
+            line_color = GRID_BOLD_LINE_COLOR_BGR
         cv2.line(image, (0, y), (image.shape[1], y), line_color, line_width)
 
 # Display the binary image
-st.image(image, caption='')
-st.write(f'vyska: {downscale_image.shape[0]}')
-st.write(f'sirka: {downscale_image.shape[1]}')
+st.image(image)
+st.write(f'Rozměry: {width_size} x {height_size}')
 
-# Produce output for crocheting 
-gray_image = (downscale_image > 128).astype(bool) # TODO: This is hacky, should be better way to do this
-# TODO: Figure out what is good output strategy
-if st.sidebar.button('Download Image'):
-    cv2.imwrite('output_image.jpg', cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
-    pdf = fpdf.FPDF()
-    pdf.add_page()
-    pdf.image('output_image.jpg', x=10, y=10, w=100, h=100)
-    pdf.output('output.pdf', 'F')
+# # Produce output for crocheting 
+# gray_image = (downscale_image > 128).astype(bool) # TODO: This is hacky, should be better way to do this
+# # TODO: Figure out what is good output strategy#
+# if st.sidebar.button('Stáhnout vygenerovanou předlohu'):
+#     cv2.imwrite('output_image.jpg', cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
+#     pdf = fpdf.FPDF()
+#     pdf.add_page()
+#     pdf.image('output_image.jpg', x=10, y=10, w=100, h=100)
+#     pdf.output('output.pdf', 'F')
